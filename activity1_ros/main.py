b65a1665a615e66fc25dc101d5a117cfee50070b
StewaneWeaulieu <stephane.beaulieu.6@ulaval.ca>
2023-02-25 02:18:03 -0500

    Update main.py
    remove untested code

--- activity1_ros/main.py
+++ activity1_ros/main.py
@@ -1,27 +1,7 @@
 import sys
 import rosbag
-import csv
-
-import rospy
-import roslaunch
-
 
 if __name__ == '__main__':
-
-	package = 'rqt_gui'
-	executable = 'rqt_gui'
-	node = roslaunch.core.Node(package, executable)
-
-	launch = roslaunch.scriptapi.ROSLaunch()
-	launch.start()
-
-	process = launch.launch(node)
-	print(process.is_alive())
-
-	process.stop()
-
-	exit()
-
 	if len(sys.argv) <= 2:
 		print("You need to enter the file name of the bag as the first argument and the topic as the second argument.")
 		exit()
@@ -31,13 +11,8 @@ if __name__ == '__main__':
 
 	bag = rosbag.Bag(bag_name)
 
-	file = open('output.csv', 'w', newline='')
-	writer = csv.writer(file)
-	writer.writerow(["x", "y", "z"])
-
 	for topic, message, time in bag.read_messages(topic_name):
 		print(message)
-
 
 
 
